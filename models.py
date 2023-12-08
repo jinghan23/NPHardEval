ff76b1951f2b87cab84f60e28a6706021a40262c
Lingyao <125314658+Lingyao1219@users.noreply.github.com>
2023-12-08 14:44:34 -0500

    Add files via upload
    Add palm_model to models.py

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import json
 import os
 import ssl
 import requests
+import time
 import pandas as pd
 
 
@@ -14,30 +15,34 @@ Close-source models
 - Claude 2 Instant, and 
 - PaLM 2
 '''
+#import anthropic
 
 from openai import OpenAI
-import anthropic
-
-# !pip install google-cloud-aiplatform;
-# from google.cloud import aiplatform
+from google.cloud import aiplatform
 import vertexai
 from vertexai.preview.language_models import TextGenerationModel, ChatModel
+
 #get_ipython().system('gcloud auth application-default login')
 
 ### Load secrets
-SECRET_FILE = '../../../secrets.txt'
+SECRET_FILE = '../../secrets.txt'
 with open('../../secrets.txt') as f:
     lines = f.readlines()
     for line in lines:
         if line.split(',')[0].strip() == "open_ai_key":
             open_ai_key = line.split(',')[1].strip()
-        if line.split(',')[0].strip() == "anthropic_key":
+        elif line.split(',')[0].strip() == "anthropic_key":
             anthropic_key = line.split(',')[1].strip()
+        elif line.split(',')[0].strip() == "palm_project_id":
+            palm_project_id = line.split(',')[1].strip()
 
 openai_client = OpenAI(api_key=open_ai_key)
-claude_client = anthropic.Anthropic(api_key=anthropic_key)
-
+# claude_client = anthropic.Anthropic(api_key=anthropic_key)
+vertexai.init(project = palm_project_id, location="us-central1")
+chat_model = ChatModel.from_pretrained("chat-bison@001")
 ### Run Models
+
+
 # GPT models (GPT-3.5 and GPT-4)
 def run_gpt(text_prompt, max_tokens_to_sample: int = 3000, temperature: float = 0, client=openai_client, model = "gpt-3.5-turbo"):
     # use gpt-3.5-turbo unless specify gpt-4
@@ -53,6 +58,7 @@ def run_gpt(text_prompt, max_tokens_to_sample: int = 3000, temperature: float = 
 
 
 # Claude 2 models (Claude 2 and Claude 2 Instant)
+"""
 def run_claude(text_prompt, max_tokens_to_sample: int = 3000, temperature: float = 0, client=claude_client, model = "claude-instant-1.2"):
     # use claude-instant unless specify claude-2
     prompt = f"{anthropic.HUMAN_PROMPT} {text_prompt}{anthropic.AI_PROMPT}"
@@ -64,46 +70,21 @@ def run_claude(text_prompt, max_tokens_to_sample: int = 3000, temperature: float
         temperature=temperature,
     ).completion
     return resp
+"""
 
 # PaLM 2 model
-class GooglePaLM():
-    
-    def __init__(self):
-        """Initiate attributes in the GooglePaLM object"""
-        self.project_id = self.read_secrets()
-        vertexai.init(project = self.project_id, location="us-central1")
-        self.chat_model = ChatModel.from_pretrained("chat-bison@001")
-        self.generation_model = TextGenerationModel.from_pretrained("text-bison@001")
-    
-    def read_secrets(self):
-        """Read PaLM project id from secrets file"""
-        project_id = None
-        with open(SECRET_FILE) as f:
-            lines = f.readlines()
-            for line in lines:
-                if line.split(',')[0].strip() == "palm_project_id":
-                    project_id = line.split(',')[1].strip()
-        return project_id
-    
-    def get_palm_generation_output(self, prompt):
-        """Use Google PaLM text generation model"""
-        response = self.generation_model.predict(
-            prompt = prompt,
-            temperature = 0
-        )
-        response = response.text
-        return response
 
-    def get_palm_chat_output(self, prompt):
-        """Use Google PaLM chat model"""
-        parameters = {
-            "temperature": 0, 
-            "max_output_tokens": 256
-        }
-        chat = self.chat_model.start_chat()
-        response = chat.send_message(prompt, **parameters)
-        response = response.text
-        return response
+def run_palm(text_prompt, max_tokens_to_sample: int = 1000, temperature: float = 0, model = "chat-bison@001"):
+    """Use Google PaLM chat model"""
+    parameters = {
+        "temperature": temperature, 
+        "max_output_tokens": max_tokens_to_sample
+    }
+    chat = chat_model.start_chat()
+    response = chat.send_message(text_prompt, **parameters)
+    response = response.text
+    time.sleep(2)
+    return response
 
 '''
 Open-source models (choose from below, in total 5 models)
@@ -181,5 +162,5 @@ Open-source models (choose from below, in total 5 models)
 
 if __name__ == '__main__':
     # try claude2-instant
-    resp = run_claude(text_prompt= "I am a human, and I am a", model="claude-2") # claude-2 works claude-instant-1.2
+    resp = run_gpt(text_prompt= "I am a human, and I am a", model="gpt-4-1106-preview") # claude-2 works claude-instant-1.2
     print(resp)
