42f6cdcb4a97a917f7702436195b81d0e8e17383
Lizhou Fan <lizhouf@umich.edu>
2023-12-10 16:37:41 -0500

    Added few-shot run files

--- run.sh
+++ run.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
+# these are the commands to run the experiments in the paper
+# we only provide examples for each model
+# we do NOT suggest running commands in this file all at once as it will take a long time
 
-# Run run_hard_GCP with different models
-python run/run_hard_GCP.py gpt-4-1106-preview &
-python run/run_hard_GCP.py gpt-3.5-turbo &
-python run/run_hard_GCP.py claude-2 &
-python run/run_hard_GCP.py claude-instant-1.2 &
+# Close source models: gpt-4-1106-preview, gpt-3.5-turbo, claude-2, claude-instant-1.2, chat-bison@001
+# Open source models: Mistral-7B-Instruct-v0.1, llm-yi-34b, vicuna-13b-v1.3, phi-1_5, Baichuan-13B-Chat
 
-# Run run_hard_MSP with different models
-python run/run_hard_MSP.py gpt-4-1106-preview &
-python run/run_hard_MSP.py gpt-3.5-turbo &
-python run/run_hard_MSP.py claude-2 &
-python run/run_hard_MSP.py claude-instant-1.2 &
+# Run run_hard_GCP with different close source models, zero-shot
+python run_close_zero/run_hard_GCP.py gpt-4-1106-preview &
+python run_close_zero/run_hard_GCP.py claude-2 &
 
-# Run run_hard_TSP with different models
-python run/run_hard_TSP.py gpt-4-1106-preview &
-python run/run_hard_TSP.py gpt-3.5-turbo &
-python run/run_hard_TSP.py claude-2 &
-python run/run_hard_TSP.py claude-instant-1.2 &
+# Run run_p_BSP with different close source models, few-shot self examples
+python run_close_few_self/run_p_BSP.py gpt-4-1106-preview self &
+python run_close_few_self/run_p_BSP.py claude-2 self &
+
+# Run run_p_MFP with different close source models, few-shot with other examples
+python run_close_few_other/run_p_MFP.py gpt-4-1106-preview other &
+python run_close_few_other/run_p_MFP.py claude-2 other &
+
+# Run run_hard_GCP with different open source models, zero-shot
+# TODO
+
+# Run run_hard_GCP with different open source models, few-shot self examples
+# TODO
+
+# Run run_hard_GCP with different open source models, few-shot with other examples
+# TODO
 
 # Wait for all background jobs to finish
 wait
