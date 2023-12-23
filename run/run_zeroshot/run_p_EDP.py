a86c303bb7d2b3679c87cfc1013ac880838045c6
Lingyao <125314658+Lingyao1219@users.noreply.github.com>
2023-12-22 19:47:40 -0500

    Update run_p_EDP.py

--- run/run_zeroshot/run_p_EDP.py
+++ run/run_zeroshot/run_p_EDP.py
@@ -43,6 +43,8 @@ def runEDP(q, p=edpPrompts):
         output = run_gpt(prompt_text, model=MODEL)
     elif 'claude' in MODEL:
         output = run_claude(prompt_text, model=MODEL)
+    elif 'bison' in MODEL:
+        output = run_palm(prompt_text, model=MODEL)
     else:
         print('Model not found')
         return None
