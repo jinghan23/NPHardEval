945fe9cb2f9cd6dcce156e20b2a79c3135b4ca7b
Jinghan Zhang <zhangcharlotte84@gmail.com>
2024-01-15 21:23:02 +0800

    add editcuda

--- editcuda.sh
+++ editcuda.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export CUDA_HOME="/ssddata/local/cuda-12.1"
+export CUDA_HOME="/GPUFS/yt_ust_junxianh_1/jhzhang/cuda-12.1"
 export PATH="${CUDA_HOME}/bin:${PATH}"
 export LD_LIBRARY_PATH="${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}"
